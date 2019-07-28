119ab894f4d785b2e8fecf57b31adc1cd3123b18
Carmel Barak <carmel.b@c3dltd.com>
2019-07-28 10:04:33 +0300

    Add project files.

---
+++ My Home Sharing/MainActivity.cs
@@ -0,0 +1,102 @@
+﻿using Android.App;
+using Android.Gms.Common;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace MyHomeSharing
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
+    {
+        static readonly string TAG = "MainActivity";
+
+        internal static readonly string CHANNEL_ID = "my_notification_channel";
+        internal static readonly int NOTIFICATION_ID = 100;
+
+        TextView textMessage;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            textMessage = FindViewById<TextView>(Resource.Id.message);
+           // BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+           // navigation.SetOnNavigationItemSelectedListener(this);
+
+            IsPlayServicesAvailable();
+
+            CreateNotificationChannel();    
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.navigation_home:
+                    textMessage.SetText(Resource.String.title_home);
+                    return true;
+                case Resource.Id.navigation_dashboard:
+                    textMessage.SetText(Resource.String.title_dashboard);
+                    return true;
+                case Resource.Id.navigation_notifications:
+                    textMessage.SetText(Resource.String.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    textMessage.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
+                else
+                {
+                    textMessage.Text = "This device is not supported";
+                    Finish();
+                }
+                return false;
+            }
+            else
+            {
+                textMessage.Text = "Google Play Services is available.";
+                return true;
+            }
+        }
+        void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification
+                // channel on older versions of Android.
+                return;
+            }
+
+            var channel = new NotificationChannel(CHANNEL_ID,
+                                                  "FCM Notifications",
+                                                  NotificationImportance.Default)
+            {
+
+                Description = "Firebase Cloud Messages appear in this channel"
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(Android.Content.Context.NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
+        }
+    }
+}
+
---
+++ My Home Sharing/SplashActivity.cs
@@ -0,0 +1,149 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Gms.Auth.Api;
+using Android.Gms.Auth.Api.SignIn;
+using Android.Gms.Common;
+using Android.Gms.Common.Apis;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Util;
+using Firebase;
+using Firebase.Auth;
+using System.Threading.Tasks;
+using Android.Widget;
+using System;
+
+namespace MyHomeSharing
+{
+    [Activity(Label = "SplashActivity", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class SplashActivity : AppCompatActivity, GoogleApiClient.IConnectionCallbacks, GoogleApiClient.IOnConnectionFailedListener, FirebaseAuth.IAuthStateListener
+    {
+        public static FirebaseApp app;
+        static GoogleApiClient mGoogleApiClient;
+
+        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+        private const int SIGN_IN_CODE = 9000;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Log.Debug(TAG, "SplashActivity.OnCreate");
+            SetContentView(Resource.Layout.activity_main);
+
+            var signinButton = FindViewById<Button>(Resource.Id.signin_button);
+            signinButton.Click += SigninButton_Click;
+            InitFirebaseAuth();
+            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DefaultSignIn)
+                .RequestEmail()
+                .RequestIdToken(""/*TODO - remove hardcoded*/)
+                .Build();
+
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .EnableAutoManage(this, this)
+                    .AddApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                    .AddConnectionCallbacks(this)
+                    .Build();
+
+
+
+        }
+
+        private async void SigninButton_Click(object sender, System.EventArgs e)
+        {
+
+            //await Auth.GoogleSignInApi.SignOut(mGoogleApiClient);
+            GoogleSignInAccount account = GoogleSignIn.GetLastSignedInAccount(this);
+            if (account == null)
+            {
+                var signInIntent = Auth.GoogleSignInApi.GetSignInIntent(mGoogleApiClient);
+                StartActivityForResult(signInIntent, SIGN_IN_CODE);
+            }
+            else
+            {
+                FirebaseLogin(account);
+            }
+        }
+
+        private async void FirebaseLogin(GoogleSignInAccount account)
+        {
+            var credentials = GoogleAuthProvider.GetCredential(account.IdToken, null);
+            var instance = FirebaseAuth.GetInstance(app);
+            if (instance == null)
+            {
+                instance = new FirebaseAuth(app);
+            }
+            var result = await instance.SignInWithCredentialAsync(credentials);
+            if (result.User != null)
+            {
+
+            }
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { LoadApplicationData(); });
+            startupWork.Start();
+        }
+
+        private async void LoadApplicationData()
+        {
+            //StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+        private void InitFirebaseAuth()
+        {
+            if (app == null)
+            {
+                app = FirebaseApp.InitializeApp(this);
+            }
+            
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            if (requestCode == SIGN_IN_CODE)
+            {
+                if (resultCode == Result.Ok)
+                {
+                    GoogleSignInResult result = Auth.GoogleSignInApi.GetSignInResultFromIntent(data);
+                    if (result.IsSuccess)
+                    {
+                        FirebaseLogin(result.SignInAccount);
+                    }
+                    Log.Debug(TAG, $"result.StatusCode: {result.Status.StatusCode}\nMeessage: {result.Status.StatusMessage}");
+                    //GoogleAuthProvider.GetCredential()
+                    //// create event listener so we know when the user has sucessfully signed in
+                    //await FirebaseAuth.Instance.SignInWithCredentialAsync(email, password);
+                }
+            }
+        }
+
+        public void OnConnected(Bundle connectionHint)
+        {
+        }
+
+        public void OnConnectionSuspended(int cause)
+        {
+        }
+
+        public void OnConnectionFailed(ConnectionResult result)
+        {
+        }
+
+
+        private void AuthStateChanged(object sender, FirebaseAuth.AuthStateEventArgs e)
+        {
+        }
+
+        public void OnAuthStateChanged(FirebaseAuth auth)
+        {
+            FirebaseUser user = FirebaseAuth.GetInstance(app).CurrentUser;
+            if (user != null)
+            {
+
+            }
+        }
+    }
+}
